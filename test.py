26567563aa603d085450b6efc4b7a5ce0562977d
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2017-11-18 16:29:51 +0200

    fix test.py bug

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import requests
 import shutil
+import httplib
 
 
 r = requests.get("http://eee.guc.edu.eg/Courses/Networks/NETW903%20Network%20and%20Services/Assessment/Assignment2.pdf", stream=True)
-print path
+print r
 if r.status_code == 200:
     with open("Assignment2.pdf", 'wb') as f:
 		r.raw.decode_content = True
